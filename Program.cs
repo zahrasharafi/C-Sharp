f1b422d894b037530d24fc2cf19d29f8742bf5d3
Zahra Sharafi <zahrasharafi72@gmail.com>
2017-03-06 18:50:09 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForLoop
+{
+    public class ForLoop
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            int j = 10;
+            for (i = 0, Console.WriteLine("Start: {0}", i); i < j; i++, j--, Console.WriteLine("i = {0} , j = {1}", i, j))
+            {
+                //Body of the loop
+            }
+            Console.ReadKey();
+        }
+    }
+}
