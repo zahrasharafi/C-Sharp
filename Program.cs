1334ba820071ceb774d72b423a0efbd0a925b811
Zahra Sharafi <zahrasharafi72@gmail.com>
2017-03-06 18:42:30 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhileLoop
+{
+    public class WhileLoop
+    {
+        static void Main(string[] args)
+        {
+            int n = 1;
+            while (n++ < 6)
+            {
+                Console.WriteLine("Current value of n is : {0} ", n);
+            }
+            Console.ReadKey();
+        }
+    }
+}
