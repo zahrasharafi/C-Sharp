3b652c866913a625e4acc0dbf2785944633bf71a
Zahra Sharafi <zahrasharafi72@gmail.com>
2017-03-06 18:47:02 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoLoop
+{
+    public class DoWhileLoop
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Do While Loop");
+            int x = 0;
+            do
+            {
+                Console.WriteLine(x);
+                x++;
+            }
+            while (x < 5);
+            Console.ReadKey();
+        }
+    }
+}
